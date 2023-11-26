886c70fe93777db67361aa3c44f3b24462941676
wkdrhkdwls <79461880+wkdrhkdwls@users.noreply.github.com>
2023-11-26 19:20:09 +0900

    Feat : install mysql

--- requestHandler.js
+++ requestHandler.js
@@ -1,10 +1,14 @@
 function main(response) {
+  console.log("main");
+
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write("Main Page");
   response.end();
 }
 
 function login(response) {
+  console.log("login");
+
   response.writeHead(200, { "Content-Type": "text/html" });
   response.write("Login Page");
   response.end();
